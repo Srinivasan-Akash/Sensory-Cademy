c20754774acd0bbdd62594145533f810ec9de04a
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-09-01 20:58:29 +0530

    Styled Preloader + Developed UI Of onBoarding + Created smooth scroll on click

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Preloader() {
   return (
-    <div>Preloader</div>
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'linear-gradient(0deg, #10141F 0%, #10141F 100%), linear-gradient(0deg, #10141F 0%, #10141F 100%), linear-gradient(0deg, #10141F 0%, #10141F 100%), #10141F',
+        color: 'white',
+        fontSize: '24px',
+        zIndex: 9999,
+      }}
+    >
+      <Image src={"/preloader.gif"} width={213} height={120} />
+    </div>
   )
 }
--- src/pages/onBoarding.js
+++ src/pages/onBoarding.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/styles/OnBoarding.module.css';
 
-export default function onBoarding() {
+export default function OnBoarding() {
+  const questions = [
+    {
+      question: "Who Are You ??",
+      options: ["Teacher", "Student", "Guardian"]
+    },
+    {
+      question: "What Is Your Gender ??",
+      options: ["Male", "Female"]
+    },
+    {
+      question: "What Is Your Disability ??",
+      options: ["Blind", "Deaf", "Dumb", "None"]
+    }
+  ];
+
+  function nextQuestion() {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    const newScrollPosition = currentScrollPosition + window.innerHeight;
+  
+    // Scroll to the new position with a smooth animation
+    window.scrollTo({
+      top: newScrollPosition,
+      behavior: "smooth"
+    });
+  }
+
+  function prevQuestion() {
+    // Calculate the new scroll position by subtracting 100vh from the current scroll position
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    const newScrollPosition = currentScrollPosition - window.innerHeight;
+  
+    // Ensure the new scroll position doesn't go below 0
+    const finalScrollPosition = Math.max(0, newScrollPosition);
+  
+    // Scroll to the new position with a smooth animation
+    window.scrollTo({
+      top: finalScrollPosition,
+      behavior: "smooth"
+    });
+  }
+
   return (
-    <div>onBoarding</div>
-  )
+    <main className={styles.container}>
+      {questions.map((element, index) => (
+        <div className={styles.question} key={index}>
+          <h1>{element.question}</h1>
+          <div className={styles.options}>
+            {element.options.map((option, optionIndex) => (
+              <div className={styles.option} key={optionIndex}>
+                <h2>{option}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+
+      <div className={styles.navigationButtons}>
+        <button onClick={prevQuestion}>Prev</button>
+        <button onClick={nextQuestion}>Next</button>
+      </div>
+    </main>
+  );
 }
