90cdefc00c24526dfad5813b3377d5f4f0306c44
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-09-06 17:58:52 +0530

    ADded utils folder

---
+++ src/utils/questions.js
@@ -0,0 +1,18 @@
+export const questions = [
+    {
+        question: "Who Are You ??",
+        options: ["Teacher", "Student", "Guardian", "Instructor"],
+    },
+    {
+        question: "What Is Your Gender ??",
+        options: ["Male", "Female"],
+    },
+    {
+        question: "What Is Your Disability ??",
+        options: ["Blind", "Deaf", "Dumb", "None"],
+    },
+    {
+        question: "How Did You Hear About Us ??",
+        options: ["Friend", "Social Medi", "Googling", "None"],
+    },
+];
